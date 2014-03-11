e98854e57012e16d2a28664ff47f77ef85f72918
trevor-umeda <FroggyP@gmail.com>
2014-03-10 22:05:17 -0700

    update

--- js/popup.js
+++ js/popup.js
@@ -20,48 +20,100 @@ var urlMap = {};
 
 
 $(function(){     
-chrome.webNavigation.onBeforeNavigate.addListener(function(detail){    
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      if(tabs[0].url.indexOf("batoto") > 0){
-        var fullTitle = tabs[0].title
-        var mangaTitle = fullTitle.split("-")[0]
-        if(!urlMap[mangaTitle]){
-            $("#links").append("<a href='#' class='url' id='"+tabs[0].url+"'>"+ mangaTitle + "</a><br>")
-        }
-        urlMap[mangaTitle] = tabs[0].url 
+  //Whenever a new page is navigated to with extension open
+  //
+  chrome.webNavigation.onBeforeNavigate.addListener(function(detail){    
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        if(tabs[0].url.indexOf("batoto") > 0){
+          var fullTitle = tabs[0].title
+          var mangaTitle = fullTitle.split("-")[0]
+          if(mangaTitle.indexOf("www.batoto") < 0){
+            if(!urlMap[mangaTitle]){
+                $("#links").append("<a href='#' class='url' id='"+tabs[0].url+"'>"+ mangaTitle + "</a><br>")
+            }
+            urlMap[mangaTitle] = tabs[0].url 
 
-        chrome.storage.sync.set({'mangaBookmark': urlMap}, function() {
-          // Notify that we saved.      
-        });  
-      }
-      
-    });
-});
+            chrome.storage.local.set({'mangaBookmark': urlMap}, function() {
+              // Notify that we saved.      
+            });  
+          }
+        }      
+      });
+  });
 
+  //SYNC STUFF dont' work atm
+  chrome.storage.sync.get("mangaSync",function(storage){
+    urlMap = storage.mangaSync
+    console.log("sync storage")
+    console.log(urlMap)
+  })
 
-  chrome.storage.sync.get("mangaBookmark",function(storage){
+  //When the page is loaded, get all the bookmarks
+  //
+  chrome.storage.local.get("mangaBookmark",function(storage){
+    //Retrieve the bookmark object from storage.
+    //
     urlMap = storage.mangaBookmark
     console.log(urlMap)
+   
+    //If it doesn't exist, then create it
+    //
     if(!urlMap){
       urlMap = {};
     }
+
+    //For each manga, create a link to it
+    //
     for (var key in urlMap) {
-    if (urlMap.hasOwnProperty(key)) {  
-      $("#links").append("<a href='#' class='url' id='"+urlMap[key]+"'>"+ key + "</a><br>")
+      if (urlMap.hasOwnProperty(key)) {         
+        $("#local_table tbody").append("<tr><td><a href='#' class='url' id='"+urlMap[key]+"'>"+ key + "</a></td><td><div class='delete' id='"+key+"''>X</div></td></tr>")          
+      }
     }
-  }
+
+    //If the extension is opened on a page, then save it with sync ( doesn't work )
+
+    chrome.storage.sync.set({"mangaSync":urlMap},function(storage){
+      console.log("saving")
+      console.log(urlMap)
+      chrome.storage.sync.get("mangaSync",function(storage){
+        urlMap = storage.mangaSync
+        console.log("Got the sync storage")
+        console.log(urlMap)
+        if(!urlMap){
+          urlMap = {};
+        }
+        for (var key in urlMap) {
+          if (urlMap.hasOwnProperty(key)) {              
+            $("#sync_table tbody").append("<tr><td><a href='#' class='url' id='"+urlMap[key]+"'>"+ key + "</a></td><td>X</td></tr>")
+          }
+        }
+      })
+    })
   })
   
-
-  
-$("#links").on('click',".url",function() {
-      //$(".url").bind("mousedown", function(e) {
-        console.log($(this).attr('id'))
-        var url = $(this).attr('id')
-        chrome.tabs.create({url: url});
-        
+  $("#local_table").on('click','.delete',function(){
+    var key = $(this).attr('id');
+    console.log(urlMap)
+    console.log(key)
+    delete urlMap[key]
+    console.log(urlMap)
+    chrome.storage.local.set({'mangaBookmark': urlMap}, function() {
+      // Notify that we saved.      
+    });
   })
 
+  //When a link is clicked, open it up in a new tab
+  //  
+  $("#links").on('click',".url",function() {        
+    var url = $(this).attr('id')
+    chrome.tabs.create({url: url});      
+  })
+  $("#links2").on('click',".url",function() {        
+    var url = $(this).attr('id')
+    chrome.tabs.create({url: url});      
+  })
+  //When add is clicked, bookmark the link
+  //
   $("#add").bind("mousedown", function(e) {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       
@@ -72,9 +124,10 @@ $("#links").on('click',".url",function() {
       }
       urlMap[mangaTitle] = tabs[0].url 
 
-      chrome.storage.sync.set({'mangaBookmark': urlMap}, function() {
+      chrome.storage.local.set({'mangaBookmark': urlMap}, function() {
         // Notify that we saved.      
       });
     });  
   });
+
 });
